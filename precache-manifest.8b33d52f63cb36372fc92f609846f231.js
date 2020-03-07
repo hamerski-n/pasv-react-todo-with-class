064f4c2e0aeb21ad8139ba129b2773183209e672
hamerski-n <58872340+hamerski-n@users.noreply.github.com>
2020-03-06 22:49:24 -0800

    Updates

---
+++ precache-manifest.8b33d52f63cb36372fc92f609846f231.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c59bae5315d488c64e09476fe891372f",
+    "url": "/pasv-react-todo-with-class/index.html"
+  },
+  {
+    "revision": "1adc2e812191886efcdc",
+    "url": "/pasv-react-todo-with-class/static/css/2.66165e39.chunk.css"
+  },
+  {
+    "revision": "40b7ba451845cf73615e",
+    "url": "/pasv-react-todo-with-class/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "1adc2e812191886efcdc",
+    "url": "/pasv-react-todo-with-class/static/js/2.769c7530.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/pasv-react-todo-with-class/static/js/2.769c7530.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40b7ba451845cf73615e",
+    "url": "/pasv-react-todo-with-class/static/js/main.3d59e5cc.chunk.js"
+  },
+  {
+    "revision": "4625d9a80b8097d500ef",
+    "url": "/pasv-react-todo-with-class/static/js/runtime-main.c7bc2483.js"
+  }
+]);
